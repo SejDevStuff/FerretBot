b712cb020ba8f544d7ed84187b35a86d4b805cf8
87FerretsDev <69016968+87FerretsDev@users.noreply.github.com>
2020-07-31 23:52:00 +0100

    Revert back to normal

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('message', message => {
   if (message.content === '~help') {
     message.author.send(":wave: Hey! I am Ferret. I am being re-written in JS for better performance so expect some commands not to work. \n\n**Who am I?**\nI am a Discord bot developed by *Tux The Penguin#5615* for 87Ferrets Servers. I am both a moderation and a fun bot!\n\n**What do I moderate?**\nI watch for users using profane language. I then take action to punish the user.\n\n**What are my commands?**\nI am still being developed, so no commands yet...");
   }
-/*  for (let i = 0; i < profaneWords.length; i++) {
+for (let i = 0; i < profaneWords.length; i++) {
   const elem = profaneWords[i];
     if (message.content.toLowerCase().includes(elem)) {
       message.delete();
@@ -23,7 +23,7 @@ client.on('message', message => {
       message.channel.send(warning);
     }
   }
-*/
+
 });
 
 client.login(process.env.DISCORD_TOKEN);
